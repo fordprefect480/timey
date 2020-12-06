912dd973dae5ced283dbc44cd39458a178a67019
Owen Symes <owen@xeppo.com.au>
2020-12-07 10:26:36 +1030

    Quickstart to test static web apps deployment

--- Program.cs
+++ Program.cs
@@ -1,21 +1,21 @@
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace Timey
+namespace quickstart
 {
     public class Program
     {
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
