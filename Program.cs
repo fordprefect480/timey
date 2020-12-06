736a985485319434723775834e2ac51187796282
Owen Symes <owen@xeppo.com.au>
2020-12-06 22:46:36 +1030

    Clear out rubbish, get datetime parsing working, look/feel

--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
 using System;
-using System.Net.Http;
 using System.Collections.Generic;
-using System.Threading.Tasks;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace timey
+namespace Timey
 {
     public class Program
     {
@@ -17,7 +17,7 @@ namespace timey
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
